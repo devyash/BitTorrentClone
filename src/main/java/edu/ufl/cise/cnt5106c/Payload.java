fcde512ca0b16d6822b6a6502e90f658e58ca31a
Jinal Mashruwala <jmashruw@users.noreply.github.com>
2017-04-08 23:44:10 -0400

    Not Required

--- src/main/java/edu/ufl/cise/cnt5106c/Payload.java
+++
@@ -1,9 +0,0 @@
-package edu.ufl.cise.cnt5106c;
-/*
-* An Payload class that */
-public class Payload extends ActualMessage {
-
-    public Payload(int type, byte[] content){
-        super(type, content);
-    }
-}
