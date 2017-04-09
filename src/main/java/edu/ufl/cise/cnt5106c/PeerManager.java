15e705b06e62cc071c51776257231e9fdb234f18
Jinal Mashruwala <jmashruw@users.noreply.github.com>
2017-04-08 20:13:57 -0400

    Updating usage of Adjacent Peers,PeerInfo deletion

--- src/main/java/edu/ufl/cise/cnt5106c/PeerManager.java
+++ src/main/java/edu/ufl/cise/cnt5106c/PeerManager.java
@@ -1,43 +1,26 @@
 package edu.ufl.cise.cnt5106c;
 
-import edu.ufl.cise.cnt5106c.conf.CommonProperties;
-import edu.ufl.cise.cnt5106c.conf.RemotePeerInfo;
-
-import java.util.ArrayList;
-import java.util.BitSet;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.Properties;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-/**
- * @author Giacomo Benincasa    (giacomo@cise.ufl.edu)
- */
 public class PeerManager implements Runnable {
 
     class OptimisticUnchoker extends Thread {
         private final int _numberOfOptimisticallyUnchokedNeighbors;
         private final int _optimisticUnchokingInterval;
-        private final List<RemotePeerInfo> _chokedNeighbors = new ArrayList<>();
-        final Collection<RemotePeerInfo> _optmisticallyUnchokedPeers =
-                Collections.newSetFromMap(new ConcurrentHashMap<RemotePeerInfo, Boolean>());
+        private final List<AdjacentPeers> _chokedNeighbors = new ArrayList<>();
+        final Collection<AdjacentPeers> _optmisticallyUnchokedPeers =
+                Collections.newSetFromMap(new ConcurrentHashMap<AdjacentPeers, Boolean>());
 
-        OptimisticUnchoker(Properties conf) {
+        OptimisticUnchoker(ReadConfig conf) {
             super("OptimisticUnchoker");
             _numberOfOptimisticallyUnchokedNeighbors = 1;
-            _optimisticUnchokingInterval = Integer.parseInt(
-                    conf.getProperty(CommonProperties.NumberOfPreferredNeighbors.toString())) * 1000;
+            _optimisticUnchokingInterval = conf.NumberOfPreferredNeighbors* 1000;
         }
 
-        synchronized void setChokedNeighbors(Collection<RemotePeerInfo> chokedNeighbors) {
+        synchronized void setChokedNeighbors(Collection<AdjacentPeers> chokedNeighbors) {
             _chokedNeighbors.clear();
             _chokedNeighbors.addAll(chokedNeighbors);
         }
@@ -65,7 +48,11 @@ public class PeerManager implements Runnable {
                     _eventLogger.updateOptimisticallyUnchokedNeighbor(LogHelper.getNeighborsAsString (_optmisticallyUnchokedPeers));
                 }
                 for (PeerManagerListener listener : _listeners) {
-                    listener.unchockedPeers(RemotePeerInfo.toIdSet(_optmisticallyUnchokedPeers));
+                    Set<Integer> ids = new HashSet<>();
+                    for (AdjacentPeers peer : _optmisticallyUnchokedPeers) {
+                        ids.add(peer._peerId);
+                    }
+                    listener.unchockedPeers(ids);
                 }
             }
         }
@@ -75,27 +62,25 @@ public class PeerManager implements Runnable {
     private final int _unchokingInterval;
     private final int _bitmapsize;
     private final EventLogger _eventLogger;
-    private final List<RemotePeerInfo> _peers = new ArrayList<>();
-    private final Collection<RemotePeerInfo> _preferredPeers = new HashSet<>();
+    private final List<AdjacentPeers> _peers = new ArrayList<>();
+    private final Collection<AdjacentPeers> _preferredPeers = new HashSet<>();
     private final OptimisticUnchoker _optUnchoker;
     private final Collection<PeerManagerListener> _listeners = new LinkedList<>();
     private final AtomicBoolean _randomlySelectPreferred = new AtomicBoolean(false);
 
-    PeerManager(int peerId, Collection<RemotePeerInfo> peers, int bitmapsize, Properties conf) {
+    PeerManager(int peerId, Collection<AdjacentPeers> peers, int bitmapsize, ReadConfig conf) {
         _peers.addAll(peers);
-        _numberOfPreferredNeighbors = Integer.parseInt(
-                conf.getProperty(CommonProperties.NumberOfPreferredNeighbors.toString()));
-        _unchokingInterval = Integer.parseInt(
-                conf.getProperty(CommonProperties.UnchokingInterval.toString())) * 1000;
+        _numberOfPreferredNeighbors = conf.NumberOfPreferredNeighbors;
+        _unchokingInterval = conf.UnchokingInterval * 1000;
         _optUnchoker = new OptimisticUnchoker(conf);
         _bitmapsize = bitmapsize;
-        _eventLogger = new EventLogger (peerId,LogHelper.getLogger());
+        _eventLogger = new EventLogger (peerId, LogHelper.getLogger());
     }
 
     synchronized void addInterestPeer(int remotePeerId) {
-        RemotePeerInfo peer = searchPeer(remotePeerId);
+        AdjacentPeers peer = searchPeer(remotePeerId);
         if (peer != null) {
-            peer.setInterested();
+            peer._interested.set(true);
         }
     }
 
@@ -104,16 +89,16 @@ public class PeerManager implements Runnable {
     }
 
     synchronized void removeInterestPeer(int remotePeerId) {
-        RemotePeerInfo peer = searchPeer(remotePeerId);
+        AdjacentPeers peer = searchPeer(remotePeerId);
         if (peer != null) {
-            peer.setNotIterested();
+            peer._interested.set(false);
         }
     }
 
-    synchronized List<RemotePeerInfo> getInterestedPeers() {
-        ArrayList<RemotePeerInfo> interestedPeers = new ArrayList<>();
-        for (RemotePeerInfo peer : _peers){
-            if(peer.isInterested()){
+    synchronized List<AdjacentPeers> getInterestedPeers() {
+        ArrayList<AdjacentPeers> interestedPeers = new ArrayList<>();
+        for (AdjacentPeers peer : _peers){
+            if(peer._interested.get()){
                 interestedPeers.add(peer);
             }
         }
@@ -121,7 +106,7 @@ public class PeerManager implements Runnable {
     }
 
     synchronized boolean isInteresting(int peerId, BitSet bitset) {
-        RemotePeerInfo peer  = searchPeer(peerId);
+        AdjacentPeers peer  = searchPeer(peerId);
         if (peer != null) {
             BitSet pBitset = (BitSet) peer._receivedParts.clone();
             pBitset.andNot(bitset);
@@ -131,14 +116,14 @@ public class PeerManager implements Runnable {
     }
 
     synchronized void receivedPart(int peerId, int size) {
-        RemotePeerInfo peer  = searchPeer(peerId);
+        AdjacentPeers peer  = searchPeer(peerId);
         if (peer != null) {
             peer._bytesDownloadedFrom.addAndGet(size);
         }
     }
 
     synchronized boolean canUploadToPeer(int peerId) {
-        RemotePeerInfo peerInfo = new RemotePeerInfo(peerId);
+        AdjacentPeers peerInfo = new AdjacentPeers(peerId, "127.0.0.1",0,false);
         return (_preferredPeers.contains(peerInfo) ||
                 _optUnchoker._optmisticallyUnchokedPeers.contains(peerInfo));
     }
@@ -148,7 +133,7 @@ public class PeerManager implements Runnable {
     }
 
     synchronized void bitfieldArrived(int peerId, BitSet bitfield) {
-        RemotePeerInfo peer  = searchPeer(peerId);
+        AdjacentPeers peer  = searchPeer(peerId);
         if (peer != null) {
             peer._receivedParts = bitfield;
         }
@@ -156,7 +141,7 @@ public class PeerManager implements Runnable {
     }
 
     synchronized void haveArrived(int peerId, int partId) {
-        RemotePeerInfo peer  = searchPeer(peerId);
+        AdjacentPeers peer  = searchPeer(peerId);
         if (peer != null) {
             peer._receivedParts.set(partId);
         }
@@ -164,16 +149,16 @@ public class PeerManager implements Runnable {
     }
 
     synchronized BitSet getReceivedParts(int peerId) {
-        RemotePeerInfo peer  = searchPeer(peerId);
+        AdjacentPeers peer  = searchPeer(peerId);
         if (peer != null) {
             return (BitSet) peer._receivedParts.clone();
         }
         return new BitSet();  // empry bit set
     }
 
-    synchronized private RemotePeerInfo searchPeer(int peerId) {
-        for (RemotePeerInfo peer : _peers) {
-            if (peer.getPeerId() == peerId) {
+    synchronized private AdjacentPeers searchPeer(int peerId) {
+        for (AdjacentPeers peer : _peers) {
+            if (peer._peerId == peerId) {
                 return peer;
             }
         }
@@ -182,10 +167,10 @@ public class PeerManager implements Runnable {
     }
 
     synchronized private void neighborsCompletedDownload() {
-        for (RemotePeerInfo peer : _peers) {
+        for (AdjacentPeers peer : _peers) {
             if (peer._receivedParts.cardinality() < _bitmapsize) {
                 // at least one neighbor has not completed
-                LogHelper.getLogger().debug("Peer " + peer.getPeerId() + " has not completed yet");
+                LogHelper.getLogger().debug("Peer " + peer._peerId + " has not completed yet");
                 return;
             }
         }
@@ -211,7 +196,7 @@ public class PeerManager implements Runnable {
 
             // 1) GET INTERESTED PEERS AND SORT THEM BY PREFERENCE
 
-            List<RemotePeerInfo> interestedPeers = getInterestedPeers();
+            List<AdjacentPeers> interestedPeers = getInterestedPeers();
             if (_randomlySelectPreferred.get()) {
                 // Randomly shuffle the neighbors
                 LogHelper.getLogger().debug("selecting preferred peers randomly");
@@ -222,15 +207,15 @@ public class PeerManager implements Runnable {
                 Collections.sort(interestedPeers, new Comparator() {
                     @Override
                     public int compare(Object o1, Object o2) {
-                        RemotePeerInfo ri1 = (RemotePeerInfo) (o1);
-                        RemotePeerInfo ri2 = (RemotePeerInfo) (o2);
+                        AdjacentPeers ri1 = (AdjacentPeers) (o1);
+                        AdjacentPeers ri2 = (AdjacentPeers) (o2);
                         // Sort in decreasing order
                         return (ri2._bytesDownloadedFrom.get() - ri1._bytesDownloadedFrom.get());
                     }
                 });
             }
 
-            Collection<RemotePeerInfo> optUnchokablePeers = null;
+            Collection<AdjacentPeers> optUnchokablePeers = null;
 
             Collection<Integer> chokedPeersIDs = new HashSet<>();
             Collection<Integer> preferredNeighborsIDs = new HashSet<>();
@@ -238,8 +223,8 @@ public class PeerManager implements Runnable {
 
             synchronized (this) {
                 // Reset downloaded bytes, but buffer them for debugging                  
-                for (RemotePeerInfo peer : _peers) {
-                    downloadedBytes.put (peer.getPeerId(), peer._bytesDownloadedFrom.longValue());
+                for (AdjacentPeers peer : _peers) {
+                    downloadedBytes.put (peer._peerId, peer._bytesDownloadedFrom.longValue());
                     peer._bytesDownloadedFrom.set(0);
                 }
 
@@ -254,9 +239,13 @@ public class PeerManager implements Runnable {
 
                 // 3) SELECT ALLE THE INTERESTED AND UNINTERESTED PEERS, REMOVE THE PREFERRED. THE RESULTS ARE THE CHOKED PEERS
 
-                Collection<RemotePeerInfo> chokedPeers = new LinkedList<>(_peers);
+                Collection<AdjacentPeers> chokedPeers = new LinkedList<>(_peers);
                 chokedPeers.removeAll(_preferredPeers);
-                chokedPeersIDs.addAll(RemotePeerInfo.toIdSet(chokedPeers));
+                Set<Integer> ids = new HashSet<>();
+                for (AdjacentPeers peer : _preferredPeers) {
+                    ids.add(peer._peerId);
+                }
+                chokedPeersIDs.addAll(ids);
 
                 // 4) SELECT ALLE THE INTERESTED PEERS, REMOVE THE PREFERRED. THE RESULTS ARE THE CHOKED PEERS THAT ARE "OPTIMISTICALLY-UNCHOKABLE"
                 if (_numberOfPreferredNeighbors >= interestedPeers.size()) {
@@ -265,15 +254,14 @@ public class PeerManager implements Runnable {
                 else {
                     optUnchokablePeers = interestedPeers.subList(_numberOfPreferredNeighbors, interestedPeers.size());
                 }
-
-                preferredNeighborsIDs.addAll (RemotePeerInfo.toIdSet(_preferredPeers));
+                preferredNeighborsIDs.addAll (ids);
             }
 
             // debug
             LogHelper.getLogger().debug("STATE: INTERESTED:" + LogHelper.getNeighborsAsString (interestedPeers));
             LogHelper.getLogger().debug("STATE: UNCHOKED (" + _numberOfPreferredNeighbors + "):" + LogHelper.getNeighborIDsAsString (preferredNeighborsIDs));
             LogHelper.getLogger().debug("STATE: CHOKED:" + LogHelper.getNeighborIDsAsString (chokedPeersIDs));
-            
+
             for (Entry<Integer,Long> entry : downloadedBytes.entrySet()) {
                 String PREFERRED = preferredNeighborsIDs.contains(entry.getKey()) ? " *" : "";
                 LogHelper.getLogger().debug("BYTES DOWNLOADED FROM  PEER " + entry.getKey() + ": "
@@ -288,13 +276,13 @@ public class PeerManager implements Runnable {
                 listener.chockedPeers(chokedPeersIDs);
                 listener.unchockedPeers(preferredNeighborsIDs);
             }
-            
+
             // 6) NOTIFY THE OPTIMISTICALLY UNCHOKER THREAD WITH THE NEW SET OF UNCHOKABLE PEERS
 
             if (optUnchokablePeers != null) {
                 _optUnchoker.setChokedNeighbors(optUnchokablePeers);
             }
         }
-        
+
     }
 }
