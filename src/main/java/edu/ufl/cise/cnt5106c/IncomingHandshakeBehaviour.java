fd960cc8fc83dd2eca0430b94a7e9439f9acd40a
Jinal Mashruwala <jmashruw@users.noreply.github.com>
2017-04-08 23:47:46 -0400

    Replacing OnlyType and Payload

--- src/main/java/edu/ufl/cise/cnt5106c/IncomingHandshakeBehaviour.java
+++ src/main/java/edu/ufl/cise/cnt5106c/IncomingHandshakeBehaviour.java
@@ -1,4 +1,5 @@
-package edu.ufl.cise.cnt5106c; /**
+package edu.ufl.cise.cnt5106c;
+/**
  * Created by Jiya on 3/30/17.
  */
 
@@ -18,21 +19,20 @@ public class IncomingHandshakeBehaviour extends DataInputStream implements Objec
     final static int REQUEST = 6;
     final static int PIECE = 7;
 
-    public IncomingHandshakeBehaviour(InputStream in) {
-        super(in);
+    public IncomingHandshakeBehaviour(InputStream in ) {
+        super( in );
     }
 
     private boolean handshakeRequest = false;
 
     @Override
     public Object readObject() throws ClassNotFoundException, IOException {
-        if(!handshakeRequest){
+        if (!handshakeRequest) {
             HandShakeMessage h = new HandShakeMessage();
             h.read(this);
             handshakeRequest = true;
             return h;
-        }
-        else{
+        } else {
             final int length = readInt();
             ActualMessage msg = createMessageWithSpecifiedType(length - 1, readInt());
             msg.read(this);
@@ -43,23 +43,23 @@ public class IncomingHandshakeBehaviour extends DataInputStream implements Objec
     public ActualMessage createMessageWithSpecifiedType(int pLength, int type) throws ClassNotFoundException {
         switch (type) {
             case CHOKE:
-                return new OnlyType(CHOKE);
+                return new ActualMessage(CHOKE, null);
             case UNCHOKE:
-                return new OnlyType(UNCHOKE);
+                return new ActualMessage(UNCHOKE, null);
             case INTERESTED:
-                return new OnlyType(INTERESTED);
+                return new ActualMessage(INTERESTED, null);
             case NOTINTERESTED:
-                return new OnlyType(NOTINTERESTED);
+                return new ActualMessage(NOTINTERESTED, null);
             case HAVE:
-                return new Payload(HAVE,new byte[pLength]);
+                return new ActualMessage(HAVE, new byte[pLength]);
             case BITFIELD:
-                return new Payload(BITFIELD,new byte[pLength]);
+                return new ActualMessage(BITFIELD, new byte[pLength]);
             case REQUEST:
-                return new Payload(REQUEST,new byte[pLength]);
+                return new ActualMessage(REQUEST, new byte[pLength]);
             case PIECE:
-                return new Payload(PIECE,new byte[pLength]);
+                return new ActualMessage(PIECE, new byte[pLength]);
             default:
-                throw new ClassNotFoundException ("message type not handled");
+                throw new ClassNotFoundException("message type not handled");
         }
     }
 }
